a1df1d4888bde2c9d9e23cb1b8beace4c95fbe41
Marcelo Quadrilatero <marceloquadrilatero@gmail.com>
2023-09-25 16:04:34 -0300

    Miley Cyrus

--- archivo.js
+++ archivo.js
@@ -4,81 +4,91 @@ const botonIzquierda        = document.querySelector(".btn-carrousel-izquierda")
 const botonDerecha          = document.querySelector(".btn-carrousel-derecha");
 const contenidoCarrousel    = document.querySelector(".contenido");
 const imagenesCarrousel     = document.querySelectorAll(".items");
-const desplegableLogin      = document.querySelector('.contenedor-login');
+
 const btnLogin              = document.querySelector('.btn-login');
+const iconoMenu             = document.querySelector('.icono-menu');
+const menuDesplegable       = document.querySelector('.menu-desplegable');
+const nav                   = document.querySelector('.nav');
+const iconoLogin            = document.querySelector('.icono-login');
+const formDesplegable      = document.querySelector('.form-desplegable');
+const loginResponsive       = document.querySelectorAll('.login-responsive')
 
 window.addEventListener("scroll", () => {
   let scroll = window.scrollY;
   imgPortada.style.top = scroll * 1 + "px";
   cuadroUnplugged.style.right = scroll * 1.2 + "px";
 });
+
+
 let contadorLogin = 0
-btnLogin.addEventListener('click',()=>{
-    if(contadorLogin === 0){
-        desplegableLogin.style.transform = `translateX(0%)`
-        contadorLogin++
-        
-    }else{
-        desplegableLogin.style.transform = `translateX(100%)`
-        contadorLogin = 0
-    }
+
+
+const loginResponsiv = (e)=>{
+  if(contadorLogin === 0){
+    formDesplegable.style.transform = `translateX(-2%)`
+    contadorLogin++
     
+}else{
+  formDesplegable.style.transform  = `translateX(104%)`
+    contadorLogin = 0
+}
+
+}
+loginResponsive.forEach((boton)=>{
+  boton.addEventListener('click',loginResponsiv)
+});
+
+let contadorDesplegable = 0;
+iconoMenu.addEventListener('click',()=>{
+  if(contadorDesplegable === 0){
+    menuDesplegable.style.transform = `translateY(0%)`
+    let colorRgba = "rgba($color: #0a0a0a, $alpha: .980)"
+    nav.style.background = "rgb(20,20,20)"
+    iconoLogin.style.visibility = "hidden"
+    contadorDesplegable++
+  }else{
+        menuDesplegable.style.transform = `translateY(-110%)`
+        nav.style.background = "#000"
+        iconoLogin.style.visibility = "visible"
+        contadorDesplegable = 0
+  }
 })
 
+
+
+// let counter = 0;
+// let widthImg = 100 / contenidoCarrousel.childElementCount;
+
+
 // function moverDerecha() {
-//     if (counter >= contenidoCarrousel.childElementCount - 1) {
-//         counter = 0;
-//     } else {
-//         counter++;
-//     }
-//     acumulador = counter * widthImg; // Actualiza acumulador
-//     contenidoCarrousel.style.transform = `translateX(-${acumulador}%)`;
-//     console.log(acumulador);
+//   if (counter > contenidoCarrousel.childElementCount - 2) {
+//     counter = 0;
+   
+//   } else {
+//     counter++;
+//   }
+//   contenidoCarrousel.style.transform = `translateX(-${counter * widthImg}%)`;
+  
 // }
 
 // function moverIzquierda() {
-//     counter--;
-//     if (counter < 0) {
-//         counter = contenidoCarrousel.childElementCount - 1;
-//     }
-//     acumulador = counter * widthImg; // Actualiza acumulador
-//     contenidoCarrousel.style.transform = `translateX(-${acumulador}%)`;
-//     console.log(counter);
+//   counter--;
+//   if (counter < 0) {
+//     counter = 2;
+//     contenidoCarrousel.style.transform = `translateX(-${counter * widthImg}%)`;
+    
+//   }
+//   contenidoCarrousel.style.transform = `translateX(-${counter * widthImg}%)`;
 // }
 
-let counter = 0;
-let widthImg = 100 / contenidoCarrousel.childElementCount;
+// function scrollAutoShop(){
+//   let scroll = window.scrollY;
+//   if(scroll == 1100){
+//     setInterval(moverDerecha,4000)
+//     console.log(scroll)
+//   }
+// }
+// window.addEventListener('scroll',scrollAutoShop)
 
-
-function moverDerecha() {
-  if (counter > contenidoCarrousel.childElementCount - 2) {
-    counter = 0;
-   
-  } else {
-    counter++;
-  }
-  contenidoCarrousel.style.transform = `translateX(-${counter * widthImg}%)`;
-  
-}
-
-function moverIzquierda() {
-  counter--;
-  if (counter < 0) {
-    counter = 2;
-    contenidoCarrousel.style.transform = `translateX(-${counter * widthImg}%)`;
-    
-  }
-  contenidoCarrousel.style.transform = `translateX(-${counter * widthImg}%)`;
-}
-
-function scrollAutoShop(){
-  let scroll = window.scrollY;
-  if(scroll == 1100){
-    setInterval(moverDerecha,4000)
-    console.log(scroll)
-  }
-}
-window.addEventListener('scroll',scrollAutoShop)
-
-botonIzquierda.addEventListener("click", moverIzquierda);
-botonDerecha.addEventListener("click", moverDerecha);
+// botonIzquierda.addEventListener("click", moverIzquierda);
+// botonDerecha.addEventListener("click", moverDerecha);
